17f9d884fc0ffd3f529d17dd699ba12ec789640e
Albert Nuñez <albertteal@gmai.com>
2023-10-06 11:56:53 -0600

    Avance en JS

--- Mokepon/JS/js.js
+++ Mokepon/JS/js.js
@@ -1,1 +1,37 @@
-alert("Hola, mundo JS")
+function iniciarJuego(){
+    let botonClaseJugador = document.getElementById('boton-selec-clase')
+    botonClaseJugador.addEventListener('click', SeleccionarClaseJugador)
+}
+
+function SeleccionarClaseJugador(){
+    let inputHerbolestero = document.getElementById('Herbolestero')
+    let inputMonje = document.getElementById('Monje')
+    let inputCataclismo = document.getElementById('Cataclismo')
+    let inputNagas = document.getElementById('Nagas')
+    let inputTauren = document.getElementById('Tauren')
+    let inputHydros = document.getElementById('Hydros')
+    let inputPaladin = document.getElementById('Paladin')
+
+
+
+
+    if (inputHerbolestero.checked){      
+        alert("Seleccionate Herbolestero")
+    } else if (inputMonje.checked){
+        alert("Seleccionate Monje")
+    } else if (inputCataclismo.checked){
+        alert("Seleccionate Cataclismo")
+    } else if (inputNagas.checked){
+        alert("Seleccionate Nagas")
+    } else if (inputTauren.checked){
+        alert("Seleccionate Tauren")
+    } else if (inputHydros.checked){
+        alert("Seleccionate Hydros")
+    } else if (inputPaladin.checked){
+        alert("Seleccionate Paladin")
+    } else{
+        alert("Selecciona una clase")
+    }
+}
+window.addEventListener('load', iniciarJuego) //Nos sirve oara avisar que inicie todo cuando ya se haya cargado
+                                                //Todo el HTML y ahora sí pasamos al JS
